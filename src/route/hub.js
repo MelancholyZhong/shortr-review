a6eced21383bface2af5215ef3972450d7e6a215
MelancholyZhong <87922354+MelancholyZhong@users.noreply.github.com>
2022-11-02 15:00:57 -0700

    Update hub.js

--- src/route/hub.js
+++ src/route/hub.js
@@ -3,6 +3,7 @@ const { getHubLinks } = require("../service/linkService");
 const express = require("express");
 const router = express.Router();
 
+//Here you used the dynamic url for different user, we should try something like this too.
 router.get("/:user", async (req, res, next) => {
   try {
     const data = await getHubLinks(req.params.user);
